156edcaf8b1c0667c95b84e68f1c6d7b5c28d781
Pedro Lugon <pedromotta20@gmail.com>
2021-03-28 05:12:47 -0300

    :sparkles: first one hour

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <>
+      <Button color="red">Botão 1</Button>
+      <Button color="blue">Botão 2</Button>
+      <Button color="green">Botão 3</Button>
+    </>
+  );
+}
+
+export default App;
